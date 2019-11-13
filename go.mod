b5e1f2e4b1b72d7bc886662b5505963862adcdaa
Rodrigo Custodio <jorocuva@gmail.com>
2019-11-12 20:41:42 -0600

    Style download

--- go.mod
+++ go.mod
@@ -3,14 +3,25 @@ module github.com/chromz/wiki-backend
 go 1.13
 
 require (
+	github.com/PuerkitoBio/goquery v1.5.0 // indirect
+	github.com/antchfx/htmlquery v1.2.0 // indirect
+	github.com/antchfx/xmlquery v1.2.0 // indirect
+	github.com/antchfx/xpath v1.1.1 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/gocolly/colly v1.2.0
+	github.com/golang/groupcache v0.0.0-20191027212112-611e8accdfc9 // indirect
 	github.com/google/uuid v1.1.1
 	github.com/julienschmidt/httprouter v1.3.0
+	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/mattn/go-sqlite3 v1.11.0
 	github.com/pkg/errors v0.8.1 // indirect
+	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
 	github.com/stretchr/testify v1.4.0 // indirect
+	github.com/temoto/robotstxt v1.1.1 // indirect
 	go.uber.org/atomic v1.4.0 // indirect
 	go.uber.org/multierr v1.2.0 // indirect
 	go.uber.org/zap v1.10.0
 	golang.org/x/crypto v0.0.0-20191011191535-87dc89f01550
+	google.golang.org/appengine v1.6.5 // indirect
 )
--- internal/ticker/ticker.go
+++ internal/ticker/ticker.go
@@ -4,9 +4,11 @@ import (
 	"database/sql"
 	"github.com/chromz/wiki-backend/pkg/log"
 	"github.com/chromz/wiki-backend/pkg/persistence"
+	"github.com/gocolly/colly"
 	"io"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
 	"regexp"
@@ -26,6 +28,7 @@ var logger = log.GetLogger()
 var db *sql.DB
 var linkRegex = regexp.MustCompile(`\[.*?\]\((.*?)\)`)
 var basePath string
+var userAgent string
 
 type file struct {
 	classID  int64
@@ -35,61 +38,181 @@ type file struct {
 }
 
 // NewTicker constructor of the synchronizer ticker
-func NewTicker(basePathFlag, directory string, pollingRate int) *Synchronizer {
+func NewTicker(basePathFlag, userAgentFlag, directory string,
+	pollingRate int) *Synchronizer {
 	db = persistence.GetDb()
 	destDir = directory
 	basePath = basePathFlag
+	userAgent = userAgentFlag
 	return &Synchronizer{
 		ticker: time.NewTicker(time.Millisecond * time.Duration(pollingRate)),
 	}
 }
 
-func parseMarkdown(procFile file, markdownText, dir, midDir string) (map[string]string, error) {
+func isRelative(testURL string) bool {
+	u, err := url.Parse(testURL)
+	return err == nil && u.Scheme == "" && u.Host == ""
+}
+
+func onStyleSheet(processedLinks map[string]string, styleCount *int,
+	midDir, fileName string, urlStruct *url.URL,
+	client *http.Client) colly.HTMLCallback {
+	resFolder := filepath.Base(fileName) + "_resources/"
+	fileName += "_resources/"
+	err := os.MkdirAll(fileName, 0700)
+	if err != nil {
+		logger.Error("Unable to create resources dir", err)
+		return nil
+	}
+	return func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		var downloadLink string
+		if isRelative(link) {
+			downloadLink = urlStruct.Scheme + "://" +
+				urlStruct.Host + link
+		} else {
+			downloadLink = link
+		}
+		if _, ok := processedLinks[link]; ok {
+			return
+		}
+		req, err := http.NewRequest("GET", downloadLink, nil)
+		if err != nil {
+			logger.Error("Unable to create request", err)
+			return
+		}
+		req.Header.Set("User-Agent", userAgent)
+		response, err := client.Do(req)
+		if err != nil {
+			logger.Error("Unable to download file", err)
+			return
+		}
+		if err != nil {
+			logger.Error("Unable to download file", err)
+			return
+		}
+
+		styleFileName := fileName
+		baseName := strconv.Itoa(*styleCount) + "_style.css"
+		dbName := basePath + midDir + resFolder + baseName
+		processedLinks[link] = dbName
+		styleFileName += baseName
+		*styleCount++
+		file, err := os.OpenFile(styleFileName, os.O_WRONLY|os.O_CREATE, 0700)
+		if err != nil {
+			logger.Error("Unable to open file", err)
+			return
+		}
+		_, err = io.Copy(file, response.Body)
+		if err != nil {
+			logger.Error("Unable to copy file", err)
+			return
+		}
+		logger.Info("Downloaded style: " + styleFileName)
+		file.Close()
+		response.Body.Close()
+	}
+}
+
+func afterScrap(fileName string,
+	processedLinks map[string]string) colly.ScrapedCallback {
+	return func(r *colly.Response) {
+		htmlContent := string(r.Body)
+		var replaces []string
+		for k, v := range processedLinks {
+			replaces = append(replaces, k)
+			replaces = append(replaces, v)
+		}
+		replacer := strings.NewReplacer(replaces...)
+		processedHTML := replacer.Replace(htmlContent)
+		file, err := os.OpenFile(fileName,
+			os.O_WRONLY|os.O_CREATE, 0700)
+		if err != nil {
+			logger.Error("Unable to open file", err)
+			return
+		}
+		file.Write([]byte(processedHTML))
+		logger.Info("Processed web page written: " + fileName)
+		file.Close()
+	}
+}
+
+func parseMarkdown(procFile file, markdownText,
+	dir, midDir string) (map[string]string, error) {
 	processedLinks := make(map[string]string)
 	linkMatches := linkRegex.FindAllStringSubmatch(markdownText, -1)
+	client := &http.Client{}
+	collector := colly.NewCollector()
 	for _, matchArray := range linkMatches {
 		if len(matchArray) < 2 {
 			continue
 		}
 		urlSplit := strings.Split(matchArray[1], " ")
 		mdURL := urlSplit[0]
+
 		if _, ok := processedLinks[mdURL]; ok {
 			continue
 		}
-
-		response, err := http.Get(mdURL)
+		urlStruct, err := url.Parse(mdURL)
 		if err != nil {
-			logger.Error("Unable to download file", err)
-			continue
+			return nil, err
 		}
+
+		req, err := http.NewRequest("GET", mdURL, nil)
+		if err != nil {
+			logger.Error("Unable to create request", err)
+			return nil, err
+		}
+		req.Header.Set("User-Agent", userAgent)
+
 		baseName := filepath.Base(mdURL)
 		extension := filepath.Ext(baseName)
 		prefix := strconv.FormatInt(procFile.classID, 10) + "_"
 		var fileName string
+		var styleCount int
 		fileName = dir + prefix + baseName
 		if extension == "" {
 			// Assume it is html
+			htmlLinks := make(map[string]string)
+			collector.OnHTML(`link[rel="stylesheet"]`,
+				onStyleSheet(
+					htmlLinks,
+					&styleCount,
+					midDir,
+					fileName,
+					urlStruct,
+					client,
+				))
 			fileName += ".html"
+			collector.OnScraped(afterScrap(fileName, htmlLinks))
+			collector.Visit(mdURL)
+		} else {
+			logger.Info(fileName)
+			response, err := client.Do(req)
+			if err != nil {
+				logger.Error("Unable to download file", err)
+				continue
+			}
+			file, err := os.OpenFile(fileName,
+				os.O_WRONLY|os.O_CREATE, 0700)
+			if err != nil {
+				logger.Error("Unable to open file", err)
+				return nil, err
+			}
+			_, err = io.Copy(file, response.Body)
+			if err != nil {
+				logger.Error("Unable to copy file", err)
+				return nil, err
+			}
+			dbName := basePath + midDir + prefix + baseName
+			if extension == "" {
+				dbName += ".html"
+			}
+			processedLinks[mdURL] = dbName
+			file.Close()
+			response.Body.Close()
 		}
-		logger.Info(fileName)
-		file, err := os.OpenFile(fileName, os.O_WRONLY|os.O_CREATE, 0700)
-		if err != nil {
-			logger.Error("Unable to open file", err)
-			return nil, err
-		}
-		_, err = io.Copy(file, response.Body)
-		if err != nil {
-			logger.Error("Unable to copy file", err)
-			return nil, err
-		}
-		dbName := basePath + midDir + prefix + baseName
-		if extension == "" {
-			dbName += ".html"
-		}
-		processedLinks[mdURL] = dbName
 
-		file.Close()
-		response.Body.Close()
 		logger.Info("Resource: " + fileName + " created")
 	}
 	return processedLinks, nil
@@ -121,7 +244,7 @@ func processMarkdown(procFile file, markdownText string) {
 	}
 	logger.Info("Saved processed file to " + processedFileName)
 	updateQuery := `
-UPDATE text_class
+		UPDATE text_class
 		SET proc_file_name = ?
 		WHERE id = ?
 	`
@@ -141,7 +264,7 @@ UPDATE text_class
 func process() {
 	logger.Info("Pulling data from database")
 	selectQuery := `
-SELECT text_class.id as class_id, course_id, grade_id, file_name
+		SELECT text_class.id as class_id, course_id, grade_id, file_name
 		FROM text_class
 		JOIN course ON course.id = text_class.course_id
 		JOIN grade ON course.grade_id = grade.id
